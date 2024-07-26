5dee4dbf47dfbf7db2e84eb1d875d013ea003bb7
Tamirlan <striped.pricier.0k@icloud.com>
2024-07-26 15:44:07 +0300

    relaese 7

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -1,19 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import apiAxiosInstance from '../service/apiAxiosInstance';
 import QuestTemplate from './QuestTemplate';
 
 function Questions() {
   const location = useLocation();
   const { id } = location.state || {};
-  const navigate = useNavigate();
 
   const [questions, setQuestions] = useState([]);
-
   const [indexQuest, setIndexQuest] = useState(0);
-
   const [load, setLoad] = useState(false);
 
+  
   async function getQuestions() {
     const { data } = await apiAxiosInstance.get(`/questions/${id}`);
     setQuestions(data);
@@ -24,45 +22,14 @@ function Questions() {
     getQuestions();
   }, []);
 
-  function nextQuestion() {
-    setIndexQuest((prev) => {
-      if (prev < questions.length - 1) {
-        return prev + 1;
-      } else {
-        navigate('/NewGame');
-      }
-    });
-  }
-
   return (
     <>
-      {load ? 
-      
-      (
-        <>
-          <div className="card">
-            <img
-              src={questions[indexQuest].img}
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <h5 className="card-title"> {questions[indexQuest].question} </h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-
-              <button
-                onClick={nextQuestion}
-                type="button"
-                className="btn btn-primary"
-              >
-                Следующий вопрос
-              </button>
-            </div>
-          </div>
-        </>
+      {load ? (
+        <QuestTemplate
+          question={questions[indexQuest]}
+          setIndexQuest={setIndexQuest}
+          questions={questions}
+        />
       ) : (
         <div>loading</div>
       )}
