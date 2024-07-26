c7f1855ad62c67a02f40f8a4c48a5abdb8e0eb35
Aygiz Khatipov <you@example.com>
2024-07-26 15:05:46 +0300

    fixIMG

--- client/src/components/QuestTemplate.jsx
+++ client/src/components/QuestTemplate.jsx
@@ -16,11 +16,6 @@ function QuestTemplate({ question, setIndexQuest, questions }) {
         <img src={question?.img} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title"> {question?.question} </h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-
           <button
             onClick={nextQuestion}
             type="button"
--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -48,11 +48,6 @@ function Questions() {
             />
             <div className="card-body">
               <h5 className="card-title"> {questions[indexQuest].question} </h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-
               <button
                 onClick={nextQuestion}
                 type="button"
